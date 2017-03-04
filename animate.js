9d7bb614e2db5d07541a9a341772e0e5c6cc0e97
Zzcnick <zzhen@stuy.edu>
2017-03-04 13:29:03 -0500

    finish, but weird artifacts for DVD

--- animate.js
+++ animate.js
@@ -15,18 +15,18 @@ console.log(bg);
 
 var count = 0;
 
-var dvd = null;
-var txt1 = null;
-var txt2 = null;
-var circle = null;
-
 // EventListener Functions
-var createDVD = function(e) {
+var DVD = function(e) {    
+    clear();
+    window.cancelAnimationFrame(rid);
+    var x, y, dx, dy;
+    x = Math.floor(Math.random() * (width - 100));
+    y = Math.floor(Math.random() * (height - 100));
+    dx = 2; dy = 2;
+
     // Rectangle
     var r = document.createElementNS(ns, "rect");
     var color = getRandomColor();
-    x = Math.floor(Math.random() * (width - 100));
-    y = Math.floor(Math.random() * (height - 100));
     r.setAttribute("x", x);
     r.setAttribute("y", y);
     r.setAttribute("width", 100);
@@ -40,6 +40,7 @@ var createDVD = function(e) {
     t1.setAttribute("x", x + 49);
     t1.setAttribute("y", y + 43);
     t1.setAttribute("font-family", "sans-serif");
+    t1.setAttribute("font-style", "italic");
     t1.setAttribute("font-size", "24px");
     t1.setAttribute("text-anchor", "middle");
     t1.setAttribute("fill", "white");
@@ -52,37 +53,91 @@ var createDVD = function(e) {
     t2.setAttribute("x", x + 49);
     t2.setAttribute("y", y + 71);
     t2.setAttribute("font-family", "sans-serif");
+    t2.setAttribute("font-style", "italic");
     t2.setAttribute("font-size", "24px");
     t2.setAttribute("text-anchor", "middle");
     t2.setAttribute("fill", "white");
     t2.textContent = "Video";
     s.appendChild(t2);
     count++;
+
+    var bounce = function() {
+	if (x < 0 || x > width - 100) {
+	    dx *= -1;
+	    r.setAttribute("fill", getRandomColor());
+	    dx > 0 ? dx += 0.2 : dx -= 0.2;
+	    dy > 0 ? dy += 0.2 : dy -= 0.2;
+	}
+	if (y < 0 || y > height - 100) {
+	    dy *= -1;
+	    r.setAttribute("fill", getRandomColor());
+	    dx > 0 ? dx += 0.2 : dx -= 0.2;
+	    dy > 0 ? dy += 0.2 : dy -= 0.2;
+	}
+	x += dx; y += dy;
+	r.setAttribute("x", x);
+	r.setAttribute("y", y);
+	t1.setAttribute("x", x + 49);
+	t1.setAttribute("y", y + 43);
+	t2.setAttribute("x", x + 49);
+	t2.setAttribute("y", y + 71);
+	rid = window.requestAnimationFrame(bounce);
+    }	
+    bounce();
 }
-var bounce = function() {
+
+var breathe = function(e) {
+    clear();
     window.cancelAnimationFrame(rid);
-    var x, y, dx, dy;
-    
-}
+    var color = getRandomColor();
+    var r = 0;
+    var dr = 0;
+    var cycle = 0;
 
-
-var addLastCircle = function() {
-    s.appendChild(lastCircle);
+    // Circle
+    var c = document.createElementNS(ns, "circle");
+    c.setAttribute("r", r);
+    c.setAttribute("cx", width / 2);
+    c.setAttribute("cy", height / 2);
+    c.setAttribute("fill", color);
+    s.appendChild(c);
     count++;
-}
 
-var addLine = function(e) {
-    var l = document.createElementNS(ns, "line");
-    var newX = e.clientX;
-    var newY = e.clientY;
-    l.setAttribute("x1", x);
-    l.setAttribute("y1", y);
-    l.setAttribute("x2", newX);
-    l.setAttribute("y2", newY);
-    l.setAttribute("stroke", "white");
-    l.setAttribute("stroke-width", "1");
-    s.appendChild(l);
+    // Text
+    var t = document.createElementNS(ns, "text");
+    t.setAttribute("x", width / 2);
+    t.setAttribute("y", height * 0.8);
+    t.setAttribute("font-family", "sans-serif");
+    t.setAttribute("font-style", "italic");
+    t.setAttribute("font-size", "24px");
+    t.setAttribute("text-anchor", "middle");
+    t.setAttribute("fill", "#202020");
+    t.textContent = "";
+    s.appendChild(t);
     count++;
+
+    var changed = false;
+    var circle = function() {
+	r += dr / 13; if (r < 0) r = 0;
+	cycle++;
+	if (cycle % 45 == 0) {
+	    if (dr > 8) {
+		dr *= -1;
+	    } else {
+		dr++;
+	    }
+	    cycle = 0;
+	}
+	c.setAttribute("r", r);
+	c.setAttribute("fill", color);
+
+	dr < 0 ?
+	    t.textContent = "Breathe Out" :
+	    t.textContent = "Breathe In";
+
+	rid = window.requestAnimationFrame(circle);
+    }
+    circle();
 }
 
 // Backend Functions
@@ -91,6 +146,9 @@ var getRandomColor = function() {
 	Math.floor(Math.random() * 256) + ',' + 
 	Math.floor(Math.random() * 256) + ')';
     return newColor;
+}
+var stop = function(e) {
+    window.cancelAnimationFrame(rid);
 }
 var clear = function(e) {
     lastCircle = null;
@@ -105,10 +163,11 @@ var removeLast = function(e) {
 
 // Adding Event Listeners
 var b1 = document.getElementById("clear");
-b1.addEventListener("click", clear);
+b1.addEventListener("click", stop);
 var b2 = document.getElementById("dvd");
-b2.addEventListener("click", createDVD);
+b2.addEventListener("click", DVD);
 var b3 = document.getElementById("circle");
+b3.addEventListener("click", breathe);
 
 // s.addEventListener("click", connect);
 
